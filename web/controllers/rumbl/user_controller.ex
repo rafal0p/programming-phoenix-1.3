d044d891937d4319f750a811eb3852438934534a
Rafal Podlipny <rafal0p@outlook.com>
2019-05-04 15:31:27 +0200

    show user details

--- web/controllers/rumbl/user_controller.ex
+++ web/controllers/rumbl/user_controller.ex
@@ -3,6 +3,11 @@ defmodule Rumbl.UserController do
 
   def index(conn, _params) do
     users = Rumbl.Repo.all(Rumbl.User)
-    render conn, "index.html", users: users
+    render(conn, "index.html", users: users)
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Rumbl.Repo.get(Rumbl.User, id)
+    render(conn, "show.html", user: user)
   end
 end
