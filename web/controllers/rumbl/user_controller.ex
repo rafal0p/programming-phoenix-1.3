bced503181f5daf1500990d50e89cb7d98b7834f
Rafal Podlipny <rafal0p@outlook.com>
2019-05-04 14:17:56 +0200

    displaying list of Users

--- web/controllers/rumbl/user_controller.ex
+++ web/controllers/rumbl/user_controller.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
-    render(conn, "index.html", users: users)
+    users = Rumbl.Repo.all(Rumbl.User)
+    render conn, "index.html", users: users
   end
 end
