5c15a9b0f7be709cb009c9b49aa8f436b3b058f8
Rafal Podlipny <rafal0p@outlook.com>
2019-05-14 18:40:50 +0200

    speed up tests by letting insecure password hashing

--- web/controllers/rumbl/auth.ex
+++ web/controllers/rumbl/auth.ex
@@ -11,7 +11,7 @@ defmodule Rumbl.Auth do
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     cond do
-      user = conn.assigns[:current_user] ->
+      _user = conn.assigns[:current_user] ->
         conn
       user = user_id && repo.get(Rumbl.User, user_id) ->
         assign(conn, :current_user, user)
