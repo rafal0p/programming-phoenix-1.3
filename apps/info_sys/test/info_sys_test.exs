3052659b71aa5377e87fa3f62b123977aeef1141
Rafal Podlipny <rafal0p@outlook.com>
2019-06-02 11:44:30 +0200

    test InfoSys.compute/2 ignoring timeouts for now

--- apps/info_sys/test/info_sys_test.exs
+++ apps/info_sys/test/info_sys_test.exs
@@ -1,8 +1,27 @@
 defmodule InfoSysTest do
   use ExUnit.Case
-  doctest InfoSys
+  alias InfoSys.Result
 
-  test "greets the world" do
-    assert InfoSys.hello() == :world
+  defmodule TestBackend do
+    def start_link(query, ref, owner, limit) do
+      Task.start_link(__MODULE__, :fetch, [query, ref, owner, limit])
+    end
+
+    def fetch("result", ref, owner, _limit) do
+      send(owner, {:results, ref, [%Result{backend: "test", text: "result"}]})
+    end
+
+    def fetch("none", ref, owner, _limit) do
+      send(owner, {:results, ref, []})
+    end
+  end
+
+  test "compute/2 with backend results" do
+    assert [%Result{backend: "test", text: "result"}] =
+             InfoSys.compute("result", backends: [TestBackend])
+  end
+
+  test "compute/2 with no backend results" do
+    assert [] = InfoSys.compute("none", backends: [TestBackend])
   end
 end
