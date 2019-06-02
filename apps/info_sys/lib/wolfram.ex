a8bb5eb49fdf26a005be37a8d40b21b4be2b2e3f
Rafal Podlipny <rafal0p@outlook.com>
2019-06-02 13:45:21 +0200

    test Wolfram happy path

--- apps/info_sys/lib/wolfram.ex
+++ apps/info_sys/lib/wolfram.ex
@@ -3,6 +3,8 @@ defmodule InfoSys.Wolfram do
   alias InfoSys.Result
   require Logger
 
+  @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
+
   def start_link(query, query_ref, owner, limit) do
     Task.start_link(__MODULE__, :fetch, [query, query_ref, owner, limit])
   end
@@ -28,7 +30,7 @@ defmodule InfoSys.Wolfram do
   end
 
   defp fetch_xml(query_str) do
-    {:ok, {_, _, body}} = :httpc.request(
+    {:ok, {_, _, body}} = @http.request(
       String.to_char_list(
         "http://api.wolframalpha.com/v2/query" <>
         "?appid=#{app_id()}" <>
